431822ae521e695947db5aac4d79f44c3c6dd49a
ijmadalena <IJMadalenA@outlook.com>
2024-10-24 18:49:35 +0200

    Refactor: Standardize string quotes and import formatting
    Standardized the use of double quotes in strings across multiple files. Adjusted import statements to adhere to PEP 8's guidelines for multi-line imports. These changes improve code consistency and readability.

--- log_recorder_app/models.py
+++ log_recorder_app/models.py
@@ -1,13 +1,14 @@
-from django.db.models import Model, CASCADE, DateTimeField, TextField, ForeignKey
+from django.db.models import (CASCADE, DateTimeField, ForeignKey, Model,
+                              TextField)
 
 from conscious_element.models.cryptek_user import CryptekUser
 
 
 # AuditLog model
 class AuditLog(Model):
-    user = ForeignKey(CryptekUser, on_delete=CASCADE, related_name='audit_logs')
+    user = ForeignKey(CryptekUser, on_delete=CASCADE, related_name="audit_logs")
     action = TextField()
     action_date = DateTimeField(auto_now_add=True)
 
     def str(self):
-        return f'Audit log for {self.user.username}'
+        return f"Audit log for {self.user.username}"
--- message_app/models/social_share.py
+++ message_app/models/social_share.py
@@ -1,12 +1,13 @@
-from django.db.models import Model, ForeignKey, CASCADE, CharField, IntegerField
+from django.db.models import (CASCADE, CharField, ForeignKey, IntegerField,
+                              Model)
 
 from library_tomb.models.post import Post
 
 
 class SocialShare(Model):
-    post = ForeignKey(Post, on_delete=CASCADE, related_name='social_shares')
+    post = ForeignKey(Post, on_delete=CASCADE, related_name="social_shares")
     platform = CharField(max_length=100)  # e.g., 'Facebook', 'Twitter', etc.
     share_count = IntegerField(default=0)
 
     def str(self):
-        return f'{self.post.title} shared on {self.platform}'
+        return f"{self.post.title} shared on {self.platform}"
