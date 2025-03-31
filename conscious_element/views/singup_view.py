533fd6b1afffc74b3ccd0739fa3c27f2661bbf9d
ijmadalena <IJMadalenA@outlook.com>
2025-03-31 12:35:34 +0200

    Add password change, reset, and confirmation templates; enhance signup form styling
    Signed-off-by: ijmadalena <IJMadalenA@outlook.com>

--- conscious_element/views/singup_view.py
+++ conscious_element/views/singup_view.py
@@ -2,7 +2,16 @@ from allauth.account.forms import SignupForm
 from allauth.account.views import SignupView
 
 
-class MyCustomSignupForm(SignupForm):
+class CustomSignupForm(SignupForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update(
+                {
+                    "class": "w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none",
+                    "placeholder": field.label,
+                }
+            )
 
     def save(self, request):
         user = super().save(request)
@@ -10,7 +19,7 @@ class MyCustomSignupForm(SignupForm):
 
 
 class CustomSignupView(SignupView):
-    form_class = MyCustomSignupForm
+    form_class = CustomSignupForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
