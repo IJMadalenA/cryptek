fe66f6b06563a6c23479744a89e93f1d13d76a62
ijmadalena <IJMadalenA@outlook.com>
2025-04-05 02:43:25 +0200

    Add SVG icons for command line, database, share, shield, and user group; update entry detail URL in templates
    Signed-off-by: ijmadalena <IJMadalenA@outlook.com>

--- cryptek/urls.py
+++ cryptek/urls.py
@@ -25,7 +25,6 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 
 from blog_app.sitemaps import EntrySitemap
-from blog_app.views import CommentView
 from blog_app.views.email_verification_view import EmailConfirmationView
 from cryptek.csp_report_view import csp_report_view
 from message_app.views.contact_me_view import ContactMeView
@@ -57,12 +56,6 @@ urlpatterns = (
         path("account/", include("user_app.urls")),
         path("blog/", include("blog_app.urls"), name="blog"),
         path("contact/", ContactMeView.as_view(), name="contact"),
-        path(
-            "entry/<slug:slug>/comment/<int:pk>/",
-            CommentView.as_view(),
-            name="put_delete_comment",
-        ),
-        path("entry/<slug:slug>/comment/", CommentView.as_view(), name="get_post_comment"),
         path("accounts/login/", CustomLoginView.as_view(), name="login"),
         path("accounts/signup/", CustomSignupView.as_view(), name="signup"),
         path("logout/", LogoutView.as_view(), name="logout"),
