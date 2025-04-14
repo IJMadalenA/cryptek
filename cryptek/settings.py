e3356056517d2fc7b2d12fc2c34f62f815821053
ijmadalena <63321821+IJMadalenA@users.noreply.github.com>
2025-04-14 17:17:52 +0200

    Update DATABASES configuration to switch to SQLite and enable atomic requests and autocommit
    Signed-off-by: ijmadalena <63321821+IJMadalenA@users.noreply.github.com>

--- cryptek/settings.py
+++ cryptek/settings.py
@@ -128,15 +128,10 @@ WSGI_APPLICATION = "cryptek.wsgi.application"  # https://docs.djangoproject.com/
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env.str("POSTGRES_DB", None),
-        "USER": env.str("POSTGRES_USER", None),
-        "PASSWORD": env.str("POSTGRES_PASSWORD", None),
-        "HOST": env.str("POSTGRES_HOST", None),
-        "PORT": env.int("POSTGRES_PORT", None),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
         "ATOMIC_REQUESTS": True,  # https://docs.djangoproject.com/es/5.1/ref/settings/#atomic-requests.
         "AUTOCOMMIT": True,  # https://docs.djangoproject.com/es/5.1/ref/settings/#autocommit.
-        "OPTIONS": {"sslmode": "require"},
     }
 }
 
