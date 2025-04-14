d3be59a5bbee4535ddda8c7f9521513b2b8f790c
ijmadalena <63321821+IJMadalenA@users.noreply.github.com>
2025-04-14 15:16:51 +0200

    Enhance database configuration to use environment variables for PostgreSQL settings
    Signed-off-by: ijmadalena <63321821+IJMadalenA@users.noreply.github.com>

--- cryptek/settings.py
+++ cryptek/settings.py
@@ -154,9 +154,15 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
+            "NAME": env.str("POSTGRES_DB"),
+            "USER": env.str("POSTGRES_USER"),
+            "PASSWORD": env.str("POSTGRES_PASSWORD"),
+            "HOST": env.str("POSTGRES_HOST"),
+            "PORT": env.int("POSTGRES_PORT"),
+            "ATOMIC_REQUESTS": True,  # https://docs.djangoproject.com/es/5.1/ref/settings/#atomic-requests.
+            "AUTOCOMMIT": True,  # https://docs.djangoproject.com/es/5.1/ref/settings/#autocommit.
         }
     }
-    DATABASES["default"] = dj_database_url.config()
 
 # CACHES = {
 #     "default": {
