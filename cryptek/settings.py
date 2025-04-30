c3a7aedd6904a5c3da7f53eb750f019d90bb4659
ijmadalena <63321821+IJMadalenA@users.noreply.github.com>
2025-04-30 11:07:29 +0200

    Simplify ADMINS setting initialization.
    Replaced list comprehension with direct environment variable retrieval for the ADMINS setting. This change streamlines configuration handling and reduces unnecessary processing.
    
    Signed-off-by: ijmadalena <63321821+IJMadalenA@users.noreply.github.com>

--- cryptek/settings.py
+++ cryptek/settings.py
@@ -51,9 +51,7 @@ DEVELOPMENT_MODE = env.bool("DEVELOPMENT_MODE")
 
 SITE_ID = 1  # https://docs.djangoproject.com/es/5.1/ref/settings/#site-id.
 
-ADMINS = [
-    tuple(admin.split(":")) for admin in env.list("ADMINS")
-]  # https://docs.djangoproject.com/es/5.1/ref/settings/#admins.
+ADMINS = env("ADMINS")  # https://docs.djangoproject.com/es/5.1/ref/settings/#admins.
 MANAGERS = ADMINS  # https://docs.djangoproject.com/es/5.1/ref/settings/#managers.
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")  # https://docs.djangoproject.com/es/5.1/ref/settings/#allowed-hosts.
